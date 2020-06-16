7e5ef3681064f37c6c1267510c3a6b6c95c1f946
dejan obradovikj <deko898@hotmail.com>
2020-06-16 13:00:31 +0200

    removed unused components

--- src/common/components/UserMenu.tsx
+++ src/common/components/UserMenu.tsx
@@ -72,6 +72,7 @@ export default function UserMenu() {
 
   return (
     <div>
+      asd
       {/* <IconButton onClick={handleClick} className={classes.menuButton}> */}
         {/* Dejan Obradovikj */}
         {/* <ArrowDropDownIcon /> */}
