b97c08643205688dd425336b2ee94a7b7f13a69f
dejan obradovikj <deko898@hotmail.com>
2020-07-19 20:01:43 +0200

    added label to doughnut

--- src/charts/Doughnut.tsx
+++ src/charts/Doughnut.tsx
@@ -4,25 +4,57 @@ import {
   Chart,
   PieSeries,
   Title,
+  Legend,
+  Tooltip,
 } from "@devexpress/dx-react-chart-material-ui";
 
 import { Animation } from "@devexpress/dx-react-chart";
+import { withStyles } from "@material-ui/core";
+import { EventTracker } from '@devexpress/dx-react-chart';
 
 const data = [
   { region: "Asia", val: 4119626293 },
   { region: "Africa", val: 1012956064 },
   { region: "Northern America", val: 344124520 },
-  { region: "Latin America and the Caribbean", val: 590946440 },
-  { region: "Europe", val: 727082222 },
-  { region: "Oceania", val: 35104756 },
 ];
+
+const legendStyles: any = () => ({
+  root: {
+    display: "flex",
+    margin: "auto",
+    flexDirection: "row",
+    padding: 0,
+  },
+});
+const legendRootBase: any = ({ classes, ...restProps }: any) => (
+  <Legend.Root {...restProps} className={classes.root} />
+);
+
+const Root: any = withStyles(legendStyles, { name: "LegendRoot" })(
+  legendRootBase
+);
+
+const legendLabelStyles: any = () => ({
+  label: {
+    whiteSpace: "nowrap",
+  },
+});
+const legendLabelBase = ({ classes, ...restProps }: any) => (
+  <Legend.Label className={classes.label} {...restProps} />
+);
+const Label: any = withStyles(legendLabelStyles, { name: "LegendLabel" })(
+  legendLabelBase
+);
 
 export const DoughnutChart: React.FunctionComponent = () => (
   <Paper>
     <Chart data={data} height={240}>
       <PieSeries valueField="val" argumentField="region" innerRadius={0.6} />
+      <Legend position="bottom" rootComponent={Root} labelComponent={Label} />
       <Title text="Orders Doughnut" />
       <Animation />
+      <EventTracker />
+      <Tooltip />
     </Chart>
   </Paper>
 );
